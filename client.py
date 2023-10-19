2d1dddcdf2d4238a331d24182231903f3ad3e222
trevordovan <trevordovan@gmail.com>
2023-10-19 02:28:28 -0400

    Implement message sending and receiving

--- client.py
+++ client.py
@@ -1,22 +1,112 @@
 import sys
 import socket
 
+global_buffer = ""
+
 DEBUG = False
 
 if "-d" in sys.argv:
     DEBUG = True
-
+    
 def debug_print(msg):
     if DEBUG:
         print(f"[client.py] {msg}")
+        
+def sendMessage(sock, message):
+    sock.send(message.encode("ascii"))
+    sock.send("\.\r\n".encode("ascii"))  # end of message indicator
+    
+def receiveMessage(sock):
+    global global_buffer
+    
+    while "\.\r\n" not in global_buffer:
+        data = sock.recv(1024).decode()
+        global_buffer += data
+
+    # Split the buffer at the first end-of-message indicator
+    message, global_buffer = global_buffer.split("\.\r\n", 1)
+    
+    return message
 
 def main(serverName, serverPort, filename, signatureFile):
     
-    # parse message file to byte array
+    messages = parseMessagesFromFile(filename)
+    signatures = loadSignaturesFromFile(signatureFile)
+    socketTCP = createTCPsocket(serverName, serverPort)
+
     try:
-        message = open(filename, "r")
+        # handshake
+        handshake = "HELLO"
+        debug_print(f"Sending HELLO handshake")
+        sendMessage(socketTCP, "HELLO")
+
+        response = receiveMessage(socketTCP)
+        debug_print(response) if DEBUG else print(response)
+        if response == "260 OK":
+            
+            # send messages
+            for msg_index, msg in enumerate(messages):
+                
+                debug_print(f"Sending DATA command")
+                sendMessage(socketTCP, "DATA")  
+                
+                debug_print(f"Sending message: {msg.strip()}")
+                sendMessage(socketTCP, msg)   
+  
+                # check status
+                status = receiveMessage(socketTCP)
+                debug_print(status) if DEBUG else print(status)
+                if status != "270 SIG":
+                    print(f"[client.py] Error: Invalid response from server: {status}. expected '270 SIG'. Exiting")
+                    sys.exit(1)
+                
+                # signatures pass/fail
+                received_signature = receiveMessage(socketTCP)
+                debug_print(received_signature) if DEBUG else print(received_signature)
+                
+                debug_print(f"Expected Signature: {signatures[msg_index]}")
+                if received_signature == signatures[msg_index]:
+                    debug_print(f"Recieved valid signature")
+                    debug_print(f"Sending PASS")
+                    sendMessage(socketTCP, "PASS")
+                else:
+                    debug_print(f"Recieved invalid signature")
+                    debug_print(f"Sending FAIL")
+                    sendMessage(socketTCP, "FAIL")
+
+                response = receiveMessage(socketTCP)
+                debug_print(response) if DEBUG else print (response)
+
+                if response != "260 OK":
+                    print(f"[client.py] Error: Invalid response after FAIL: {response} expected '260 OK'")
+                    sys.exit(1)
+                
+            debug_print(f"Sending QUIT")
+            sendMessage(socketTCP, "QUIT")
+        else:
+            print(f"[client.py] Connection error: Invalid handshake response: {response} Exiting")
+            sys.exit(1)
+            
+    except Exception as e:
+        print(f"[client.py] Error occurred: {e}")
+    finally:
+        socketTCP.close()
+        
+def createTCPsocket(serverName, serverPort):
+    try:
+        socketTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socketTCP.connect((serverName, int(serverPort)))
+    except socket.error as e:
+        print(f"[client.py] Socket error: {e}")
+        sys.exit(1)
+        
+    return socketTCP
+        
+def parseMessagesFromFile(fileName):
+    try:
+        message = open(fileName, "r")
     except FileNotFoundError:
-        print(f"[client.py] Error: Unable to open message file '{filename}'")
+        print(f"[client.py] Error: Unable to open message file '{fileName}'")
         sys.exit(1)
 
     # parse messages into array of byte strings
@@ -25,14 +115,16 @@ def main(serverName, serverPort, filename, signatureFile):
     while line:
         length = int(line)  
         msg = message.read(length)  
-        messages.append(msg.encode("ascii"))  
+        messages.append(msg)  
         line = message.readline().strip() 
+    
+    return messages
 
-    # parse signatures into array
+def loadSignaturesFromFile(fileName):
     try:
-        signature = open(signatureFile, "r")
+        signature = open(fileName, "r")
     except FileNotFoundError:
-        print(f"[client.py] Error: Unable to open signature file '{signatureFile}'")
+        print(f"[client.py] Error: Unable to open signature file '{fileName}'")
         sys.exit(1)
 
     signatures = []
@@ -41,72 +133,8 @@ def main(serverName, serverPort, filename, signatureFile):
         if not line:
             break
         signatures.append(line)
-
-    # create TCP socket
-    try:
-        socketTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socketTCP.connect((serverName, int(serverPort)))
-    except socket.error as e:
-        print(f"[client.py] Socket error: {e}")
-        sys.exit(1)
-
-    try:
-        # send handshake
-        handshake = "HELLO"
-        debug_print(f"Sending HELLO handshake")
-        socketTCP.send(handshake.encode("ascii"))
-        handshakeRecv = socketTCP.recv(1024)
-        if handshakeRecv.decode("ascii") == "260 OK":
-            debug_print(f"Recieved 260 OK")
-            
-            # send messages
-            counter = 0
-            for msg_index, msg in enumerate(messages):
-                counter += 1
-                debug_print(f"Sending DATA command")
-                socketTCP.send("DATA".encode("ascii"))  
-                
-                debug_print(f"Sending message: {msg.decode('ascii')}")
-                socketTCP.send(msg)
-                
-                debug_print(f"Sending end-of-message indicator")
-                socketTCP.send("\.\r\n".encode("ascii"))  # send the end of message indicator
-  
-                # check status
-                if socketTCP.recv(1024).decode("ascii") != "270 SIG":
-                    print("[client.py] Error: Invalid response from server, expected '270 SIG'. Exiting")
-                    sys.exit(1)
-                debug_print(f"Recieved 270 SIG")
-                
-                # signatures pass/fail
-                received_signature = socketTCP.recv(1024).decode("ascii")
-                debug_print(f"Received Signature: {received_signature}")
-                debug_print(f"Expected Signature: {signatures[msg_index]}")
-                if received_signature == signatures[msg_index]:
-                    debug_print(f"Recieved valid signature")
-                    debug_print(f"Sending PASS")
-                    socketTCP.send("PASS".encode("ascii"))
-                else:
-                    debug_print(f"Recieved invalid signature")
-                    debug_print(f"Sending FAIL")
-                    socketTCP.send("FAIL".encode("ascii"))
-                    if socketTCP.recv(1024).decode("ascii") != "260 OK":
-                        print("[client.py] Error: Invalid response after FAIL, expected '260 OK'")
-                        sys.exit(1)
-                    debug_print(f"Recieved 260 OK") 
-                        
-                counter += 1
-                
-            debug_print(f"Sending QUIT")
-            socketTCP.send("QUIT".encode("ascii"))
-        else:
-            print("[client.py] Connection error: Invalid handshake response. Exiting")
-            sys.exit(1)
-            
-    except Exception as e:
-        print(f"[client.py] Error occurred: {e}")
-    finally:
-        socketTCP.close()
+        
+    return signatures
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
--- server.py
+++ server.py
@@ -2,6 +2,8 @@ import sys
 import socket
 import hashlib
 
+global_buffer = ""
+
 DEBUG = False
 
 if "-d" in sys.argv:
@@ -10,25 +12,119 @@ if "-d" in sys.argv:
 def debug_print(msg):
     if DEBUG:
         print(f"[server.py] {msg}")
-        
-def read_until_end(client_socket):
-    data = ""
-    while True:
-        chunk = client_socket.recv(1024).decode("ascii")
-        data += chunk
-        if data.endswith("\.r\n"):
-            break
-    return data[:-3]  # Remove the end-of-message indicat
+         
+def sendMessage(sock, message):
+    sock.send(message.encode("ascii"))
+    sock.send("\.\r\n".encode("ascii"))  # end of message indicator
+    
+def receiveMessage(sock):
+    global global_buffer
+    
+    while "\.\r\n" not in global_buffer:
+        data = sock.recv(1024).decode()
+        global_buffer += data
 
-# EXAMPLE
-# hasher.update("Ethernet cables are like the roads of the Internet, carrying data traffic to its destinations.".encode('ascii'))
-# hasher.update("186e3bdc82d21a2682ee8e82eb5fe868".encode('ascii'))
-# signature = hasher.hexdigest()
-# debug_print(f"Manual first signature: {signature}")
+    # Split the buffer at the first end-of-message indicator
+    message, global_buffer = global_buffer.split("\.\r\n", 1)
+    
+    return message
 
 def main(listenPort, fileName):
     
-    # populate keys array
+    keys = loadKeysFromFile(fileName)
+    
+    serverSocket = createTPCsocket(listenPort)
+    serverSocket.listen(1)
+    debug_print("Server is listening for connections...")
+
+    client_socket, client_address = serverSocket.accept()
+    debug_print(f"Connection from {client_address}")
+
+    try:
+        # HELLO handshake
+        message = receiveMessage(client_socket)
+        debug_print(message) if DEBUG else print(message)
+        if message != "HELLO":
+            print("[server.py] Error: Invalid handshake")
+            serverSocket.close()
+            sys.exit(1)
+
+        debug_print("Sending 260 OK")
+        sendMessage(client_socket, "260 OK")
+
+        # message loop
+        key_index = 0
+        while True:
+            
+            message = receiveMessage(client_socket)
+            debug_print(message) if DEBUG else print(message)
+            
+            if message == "DATA":
+                message = receiveMessage(client_socket).strip()
+                debug_print(message) if DEBUG else print(message)
+                
+                # generate sha256 hash
+                hasher = hashlib.sha256()
+                
+                debug_print(f"Adding message to hash: {message}")
+                hasher.update(message.encode("ascii"))
+                
+                debug_print(f"Adding key to hash: {keys[key_index]}")
+                hasher.update(keys[key_index].encode('ascii'))
+                
+                signature = hasher.hexdigest()
+                key_index += 1
+                
+                # Compare generate hash with client
+                debug_print("Sending 270 SIG")
+                sendMessage(client_socket, "270 SIG")
+                
+                debug_print(f"Sending computed signature: {signature}")
+                sendMessage(client_socket, signature)
+ 
+                response = receiveMessage(client_socket)
+                debug_print(response) if DEBUG else print(response)
+
+                if response == "PASS":
+                    debug_print("Received PASS")
+                elif response == "FAIL":
+                    debug_print("Received FAIL. Sending 260 OK")
+                else:
+                    print("[server.py] Error: Unexpected response from client")
+                    client_socket.close()
+                    sys.exit(1)
+
+                sendMessage(client_socket, "260 OK")
+                
+            elif message == "QUIT":
+                debug_print("Client initiated a quit. Closing connection.")
+                client_socket.close()
+                break
+            else:
+                print(f"[server.py] Error: Invalid command received: {message}. Exiting")
+                client_socket.close()
+                sys.exit(1)
+            
+    except Exception as e:
+        print(f"[server.py] Error occurred: {e}")
+    finally:
+        serverSocket.close()
+        
+def createTPCsocket(listenPort):
+    try:
+        serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
+        # for testing, allows to reuse socket if it wasnt properly closed
+        if DEBUG:
+            serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+            
+        serverSocket.bind(("localhost", int(listenPort)))
+        return serverSocket
+    except socket.error as e:
+        print(f"[server.py] Socket error: {e}")
+        sys.exit(1)
+        
+def loadKeysFromFile(fileName):
     try:
         keyFile = open(fileName, "r")
     except FileNotFoundError:
@@ -41,109 +137,8 @@ def main(listenPort, fileName):
         if not line:
             break
         keys.append(line)
-
-    # create TCP socket
-    try:
-        serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # for testing, allows to reuse socket if it wasnt properly closed
-        serverSocket.bind(("localhost", int(listenPort)))
-    except socket.error as e:
-        print(f"[server.py] Socket error: {e}")
-        sys.exit(1)
-
-    # wait for client connection
-    serverSocket.listen(1)
-    debug_print("Server is listening for connections...")
-
-    client_socket, client_address = serverSocket.accept()
-    debug_print(f"Connection from {client_address}")
-
-    try:
-        # HELLO handshake
-        handshake = client_socket.recv(1024)
-        if handshake.decode("ascii") != "HELLO":
-            print("[server.py] Error: Invalid handshake")
-            serverSocket.close()
-            sys.exit(1)
-        debug_print("Recieved HELLO")
-
-        client_socket.sendall("260 OK".encode("ascii"))
-
-        # message loop
-        debug_print("Entering message loop")
-        key_index = 0
-        while True:
-            
-            data = client_socket.recv(1024).decode("ascii")
-            if data == "DATA":
-                debug_print(f"Received data command from {client_address}")
-                
-                hasher = hashlib.sha256()
-                
-                # recieve a message
-                while True:
-                    line = client_socket.recv(1024).decode("ascii")
-                    debug_print(f"Received line: {line}")
-                    
-                    # Check for the escaped end-of-message code
-                    if (not line) or (line  == "\.\r\n"):
-                        break
-                    
-                    # Unescape the line
-                    #unescaped_line = line.replace("\\.", ".").replace("\\\\", "\\")
-                    unescaped_line = line.replace("\\.", "").strip()
-                    
-                    debug_print(f"Adding line: {unescaped_line}")
-                    hasher.update(unescaped_line.encode('ascii'))
-                    
-                    # Additional check for the escaped end-of-message code
-                    if (line.endswith("\.\r\n")):
-                        break
-                
-                # Compute hash from message and key
-                debug_print(f"Adding key: {keys[key_index]}")
-                hasher.update(keys[key_index].encode('ascii'))
-                signature = hasher.hexdigest()
-                key_index += 1
-                
-                debug_print("Sending 270 SIG")
-                client_socket.sendall("270 SIG".encode("ascii"))
-                
-                debug_print(f"Sending computed signature: {signature}")
-                client_socket.sendall(signature.encode("ascii"))
-
-                # Check for PASS/FAIL
-                response = client_socket.recv(1024).decode("ascii")
-                if response == "PASS":
-                    debug_print("Received PASS command from client")
-                elif response == "FAIL":
-                    debug_print("Received FAIL command from client")
-                else:
-                    print("[server.py] Error: Unexpected response from client")
-                    client_socket.close()
-                    sys.exit(1)
-                
-                debug_print("Sending 270 SIG")
-                client_socket.sendall("270 SIG".encode("ascii"))
-
-            elif data == "QUIT":
-                debug_print("Client initiated a quit. Closing connection.")
-                client_socket.close()
-                break
-            elif data == "FAIL":
-                debug_print("Recieved FAIL. Sending 260 OK")
-                client_socket.send("260 OK".encode("ascii"))
-            else:
-                print(f"[server.py] Error: Invalid command received: {data} Exiting")
-                client_socket.close()
-                sys.exit(1)
-             
-        debug_print("Exiting message loop")
-            
-    except Exception as e:
-        print(f"[server.py] Error occurred: {e}")
-    finally:
-        serverSocket.close()
+        
+    return keys
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
